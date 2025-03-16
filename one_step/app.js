fd5c846ea708c67f2eb1016f84aa3d90a55c4817
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-16 11:25:13 +0100

    Better_code
    Improved code for "hello world'

--- one_step/app.js
+++ one_step/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+// const printMessage = (message = 'Hello World') =>{
+// 	console.log(`${message}`);
+// };
+// printMessage();
+// printMessage('Welcome to JavaScript!');
+
+const printMessage = (message = 'Hello, I am Dawid ') =>{
+	console.log(`${message}`);
+};
+printMessage();
+printMessage('write JavaScript Coding');
